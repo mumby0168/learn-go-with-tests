fa2759f019379a96ebd7a9465df91972d62b7298
Billy Mumby <billy.mumby@outlook.com>
2024-04-17 16:17:16 +0100

    feat: handling an empty name

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name
+
+	if name == "" {
+		return "Hello world"
+	}
+
+	return englishHelloPrefix + name
 }
 
 func main() {
